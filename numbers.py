901df20478e5a7d7f868f190174073e793807c8d
hoangthinhn634-cmd <hoangthinhn634@gmail.com>
2026-01-12 11:25:40 +0700

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+total = a + b + c
+product = a * b * c
+average = total / 3
+
+print("Sum:", total)
+print("Product:", product)
+print("Average:", average)
